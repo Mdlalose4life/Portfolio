7e023613d76e9dd3fec976b6c4c816c4e99a84d8
Mdlalose4life <blessing1059@gmail.com>
2024-05-22 17:22:38 +0200

    Percentage

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'My React App',
+        short_name: 'ReactApp',
+        description: 'My React App using Vite',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  server: {
+    host: '0.0.0.0',  // Allow connections from external devices
+    port: 3000,  // Specify the port to run the development server
+  },
+});
